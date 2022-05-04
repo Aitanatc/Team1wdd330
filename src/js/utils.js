5bf3e7dd2df3c44bd2cf7944b55f5b407c2131ed
Aitanantc <aitanantc@gmail.com>
2022-05-03 22:55:30 -0500

    w2

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,9 +1,7 @@
 // wrapper for querySelector...returns matching element
-export function qs(selector, parent = document) {
-  return parent.querySelector(selector);
+export function qs(selector) {
+  return document.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
