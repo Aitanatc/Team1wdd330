f0248efd31a4f563831ad27a23a730a53bbbcd41
Aitanantc <aitanantc@gmail.com>
2022-05-04 22:36:15 -0500

    ..

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,7 @@
-function getLocalStorage(a) {
-  return JSON.parse(localStorage.getItem(a));
-}
-function getCartContents() {
-  const a = getLocalStorage("so-cart"),
-    r = a.map((c) => renderCartItem(c));
-  document.querySelector(".product-list").innerHTML = r.join("");
-}
-function renderCartItem(a) {
-  const r = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${a.Image}"
-      alt="${a.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${a.Name}</h2>
-  </a>
-  <p class="cart-card__color">${a.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${a.FinalPrice}</p>
+import { loadHeaderFooter } from './utils.js';
+import CartList from './cartList.js';
 
-</li>`;
-  return console.log(r), r;
-}
-getCartContents();
+loadHeaderFooter();
 
+const cart = new CartList('so-cart', document.querySelector('.product-list'));
+cart.init();
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
-import ProductData from "./productData";
-import ProductList  from "./productList";
+import ProductData from './productData.js';
+import ProductList from './productList.js';
+import { loadHeaderFooter } from './utils.js';
 
-//instance of ProductData
-const dataSource = new ProductData("tents");
-//get element to render
-const listElement = document.querySelector(".product-list");
-// instance of productlist
-const list = new ProductList("tents", dataSource, listElement);
-// cal the method to show products
-list.init();
+loadHeaderFooter();
+
+// first create an instance of our ProductData class.
+const dataSource = new ProductData('tents');
+// then get the element we want the product list to render in
+const listElement = document.querySelector('.product-list');
+// then create an instance of our ProductList class and send it the correct information.
+const myList = new ProductList('tents', dataSource, listElement);
+// finally call the init method to show our products
+myList.init();
